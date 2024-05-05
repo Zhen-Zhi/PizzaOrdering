7f20bab482209c6cef2908869a5c40d0b672333e
Ooi Zhen Zhi <zhenzhi0812@gmail.com>
2024-05-05 22:40:51 +0800

    Frontend menu, cart done

--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -4,17 +4,23 @@ import { StatusBar } from 'expo-status-bar'
 
 import { useCart } from '@/provider/CartProvider'
 import CartListItem from '@/components/CartListItem'
+import Button from '@/components/Button'
 
 const CartScreen = () => {
-  const { items } = useCart()
+  const { items, total } = useCart()
 
   return (
-    <View>
+    <View className='p-3 flex-1'>
       <FlatList 
         data={items}
         renderItem={({ item }) => <CartListItem cartItem={ item } />}
-        contentContainerStyle = {{padding: 10, gap: 10}}
+        contentContainerStyle = {{ gap: 10}}
       />
+
+      <View className='mt-auto'>
+        <Text className='text-lg font-medium'>Total: ${total}</Text>
+        <Button text='checkout' />
+      </View>
 
       <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
     </View>
--- src/provider/CartProvider.tsx
+++ src/provider/CartProvider.tsx
@@ -7,18 +7,29 @@ type CartType = {
   items: CartItem[]
   addItem: (product: Product, size: CartItem['size']) => void
   updateQuantity: (itemId: string, amount: 1 | -1) => void
+  total: number
 }
 
 const CartContext = createContext<CartType>({
   items: [],
   addItem: () => {},
-  updateQuantity: () => {}
+  updateQuantity: () => {},
+  total: 0
 })
 
 const CartProvider = ({ children }: PropsWithChildren) => {
   const [items, setItems] = useState<CartItem[]>([])
 
   const addItem = (product: Product, size: CartItem['size']) => {
+    const existingItem = items.find(
+      (item) => item.product == product && item.size == size
+    )
+
+    if(existingItem) {
+      updateQuantity(existingItem.id, 1)
+      return
+    }
+
     const newCartItem: CartItem = {
       id: randomUUID(),
       product: product,
@@ -31,11 +42,21 @@ const CartProvider = ({ children }: PropsWithChildren) => {
   }
 
   const updateQuantity = (itemId: string, amount: 1 | -1) => {
-    console.log(itemId, amount)
+    setItems(
+      items.map((item) => (
+        item.id != itemId
+          ? item
+          : { ...item, quantity: item.quantity + amount }
+      )).filter((item) => item.quantity > 0)
+    )
   }
 
+  const total = items.reduce(
+    (sum, item) => (sum += item.product.price * item.quantity), 0
+  )
+
   return (
-    <CartContext.Provider value={{ items, addItem, updateQuantity }}>
+    <CartContext.Provider value={{ items, addItem, updateQuantity, total }}>
       {children}
     </CartContext.Provider>
   )
