60cff18f512d15f181edaea8be05e7531b030997
Ooi Zhen Zhi <zhenzhi0812@gmail.com>
2024-05-10 14:04:40 +0800

    top tab bar

--- src/app/(admin)/orders/[id].tsx
+++ src/app/(admin)/orders/[id].tsx
@@ -5,6 +5,8 @@ import { Stack, useLocalSearchParams } from 'expo-router'
 import OrderListItem from '@/components/OrderListItem';
 import orders from '@asset/data/orders';
 import OrderItemListItem from '@/components/OrderItemListItem';
+import { OrderStatusList } from '@/types';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 
 const OrderListItemDetailsScreen = () => {
   const { id } = useLocalSearchParams();
@@ -23,6 +25,36 @@ const OrderListItemDetailsScreen = () => {
         data={order.order_items}
         renderItem={({ item }) => <OrderItemListItem orderDetails={item} />}
         contentContainerStyle = {{ gap: 10, padding: 10 }}
+        ListFooterComponent={
+          <>
+            <Text style={{ fontWeight: 'bold' }}>Status</Text>
+            <View style={{ flexDirection: 'row', gap: 5 }}>
+              {OrderStatusList.map((status) => (
+                <Pressable
+                  className={`border-sky-700 ${order.status == status ? 'bg-sky-700' : 'bg-transparent'}`}
+                  key={status}
+                  onPress={() => console.warn('Update status')}
+                  style={{
+                    borderWidth: 1,
+                    padding: 10,
+                    borderRadius: 5,
+                    marginVertical: 10,
+                  }}
+                >
+                  <Text
+                    className={`${order.status == status ? 'text-white' : 'text-sky-700'}`}
+                    // style={{
+                    //   color:
+                    //     order.status === status ? 'white' : Colors.light.tint,
+                    // }}
+                  >
+                    {status}
+                  </Text>
+                </Pressable>
+              ))}
+            </View>
+          </>
+        }
       />
     </View>
   )
